130e5999cdea2bf73fed491a2e0078f675c43e29
Mathurin <115731176+mathurinhauville@users.noreply.github.com>
2025-02-05 11:49:54 +0100

    call breakpoints method

--- src/main/java/com/ubo/debug/commands/BreakpointsCommand.java
+++ src/main/java/com/ubo/debug/commands/BreakpointsCommand.java
@@ -1,17 +1,11 @@
 package com.ubo.debug.commands;
 
-import com.sun.jdi.Location;
-import com.sun.jdi.request.BreakpointRequest;
 import com.ubo.debug.BreakpointManager;
 import com.ubo.debug.ScriptableDebugger;
 
 public class BreakpointsCommand implements DebuggerCommand {
     public void execute(ScriptableDebugger debugger) {
         BreakpointManager breakpointManager = debugger.getBreakpointManager();
-
-        for (BreakpointRequest bpReq : breakpointManager.getBreakpoints()) {
-            Location location = bpReq.location();
-            System.out.println("Breakpoint at " + location.declaringType().name() + ":" + location.lineNumber());
-        }
+        breakpointManager.listBreakpoints();
     }
 }
