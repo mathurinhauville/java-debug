3d9b2652b9238f9e6e7bdce8ad82926251500324
Mathurin <115731176+mathurinhauville@users.noreply.github.com>
2025-02-05 12:10:25 +0100

    import

--- src/main/java/com/ubo/debug/JDISimpleDebugger.java
+++ src/main/java/com/ubo/debug/JDISimpleDebugger.java
@@ -1,7 +1,5 @@
 package com.ubo.debug;
 
-import com.sun.jdi.VirtualMachine;
-
 public class JDISimpleDebugger {
     public static void main(String[] args) throws Exception {
         ScriptableDebugger debuggerInstance = new ScriptableDebugger();
